9590e4bb21fabcfbaa64b24cb4e5f20b18474ccb
Dave Nottage <davidn@radsoft.com.au>
2017-05-17 06:48:08 +0930

    Added DW.REST.Json.Helpers

--- KastriFree.dpr
+++ KastriFree.dpr
@@ -3,7 +3,8 @@ program KastriFree;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  DW.OSLog in 'Core\DW.OSLog.pas';
+  DW.OSLog in 'Core\DW.OSLog.pas',
+  DW.REST.Json.Helpers in 'Core\DW.REST.Json.Helpers.pas';
 
 {$R *.res}
 
