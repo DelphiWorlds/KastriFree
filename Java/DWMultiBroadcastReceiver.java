33ed4b4140600d8fd6e728a151f5b8fec9cb3007
DelphiWorlds <dave@davenottage.com>
2017-09-21 13:37:26 +0930

    Added source and binary for DWMultiBroadcastReceiver

---
+++ Java/DWMultiBroadcastReceiver.java
@@ -0,0 +1,88 @@
+package com.delphiworlds.kastri;
+
+/*******************************************************
+ *                                                     *
+ *                  Kastri Free                        *
+ *                                                     *
+ *         DelphiWorlds Cross-Platform Library         *
+ *                                                     *
+ *******************************************************/
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.os.Bundle;
+import android.util.Log;
+
+public class DWMultiBroadcastReceiver extends BroadcastReceiver {
+
+  private static final String TAG = "DWMultiBroadcastReceiver";
+
+  private static final String KEY_RESTART_AFTER_REPLACE = "DWMultiBroadcastReceiver.KEY_RESTART_AFTER_REPLACE"; // true or false
+  private static final String KEY_START_ON_BOOT = "DWMultiBroadcastReceiver.KEY_START_ON_BOOT"; // true or false
+  private static final String KEY_START_SERVICE_ON_BOOT = "DWMultiBroadcastReceiver.KEY_START_SERVICE_ON_BOOT"; // string = service name
+
+  public static final String ACTION_SERVICE_ALARM = "com.delphiworlds.kastri.DWMultiBroadcastReceiver.ACTION_SERVICE_ALARM";
+
+  private boolean startApp(Context context) {
+    context.startActivity(context.getPackageManager().getLaunchIntentForPackage(context.getPackageName()));
+    return true;
+  }
+
+  private boolean checkStartupIntent(Context context, Intent intent) {
+    // Retrieve the metadata
+    Bundle metaData = null;
+    try {
+      metaData = context.getPackageManager().getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA).metaData;
+    } catch (PackageManager.NameNotFoundException  exception) {
+      return false;
+    }
+    Boolean handled = false;
+
+    // This action will start the application if it has been updated, e.g. when "side-loaded".
+    // Note: If your app is on Google Play, you will likely want to omit having the option in the metadata
+    if (intent.getAction().equals(Intent.ACTION_MY_PACKAGE_REPLACED)) {
+      if ((metaData != null) && metaData.containsKey(DWMultiBroadcastReceiver.KEY_RESTART_AFTER_REPLACE)) {
+        if (metaData.getBoolean(DWMultiBroadcastReceiver.KEY_RESTART_AFTER_REPLACE))
+          return startApp(context);
+      }
+    }
+
+    // This action will start the application or service on bootup of the device
+    // Note: This action also needs <uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED" />  in the manifest    
+    if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+      if ((metaData != null) && metaData.containsKey(DWMultiBroadcastReceiver.KEY_START_ON_BOOT)) {
+        if (metaData.getBoolean(DWMultiBroadcastReceiver.KEY_START_ON_BOOT))
+          return startApp(context);
+      }
+      if ((metaData != null) && metaData.containsKey(DWMultiBroadcastReceiver.KEY_START_SERVICE_ON_BOOT)) {
+        String serviceName = "com.embarcadero.services." + metaData.getString(DWMultiBroadcastReceiver.KEY_START_SERVICE_ON_BOOT);
+        Log.v(TAG, "Attempting to start service from boot: " + serviceName);
+        Intent serviceIntent = new Intent();
+        serviceIntent.setClassName(context.getPackageName(), serviceName);
+        context.startService(serviceIntent);
+        return true;
+      }
+    }
+
+    // Starting a service from an alarm. The intent should already have the class name set in the intent
+    if (intent.getAction().equals(ACTION_SERVICE_ALARM)) {
+      Log.v(TAG, "Attempting to start service from alarm");
+      intent.setClassName(context.getPackageName(), intent.getStringExtra("ServiceName"));
+      context.startService(intent);
+      return true;
+    }
+
+    return false;
+  }
+
+  public void onReceive(Context context, Intent intent) {
+    Log.v(TAG, "Received intent with action: " + intent.getAction());
+    if (!checkStartupIntent(context, intent))
+      // Simply forward on the intent in a local broadcast
+      LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+  }
+
+}
