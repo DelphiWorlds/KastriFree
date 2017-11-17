f93bbdb58995c130b67c935f360368bc8ec1e8be
DelphiWorlds <dave@davenottage.com>
2017-11-17 15:08:51 +1030

    Added some help text

--- Java/DWMultiBroadcastReceiver.java
+++ Java/DWMultiBroadcastReceiver.java
@@ -8,6 +8,36 @@ package com.delphiworlds.kastri;
  *                                                     *
  *******************************************************/
 
+/*
+  DWMultiBroadcastReceiver can assist with starting your app after an update, or on boot. In order for the action handlers to start your app,
+  you will need to add the corresponding permission, receiver and metadata items in the manifest, e.g:
+
+  <!-- **** Required for start on boot  **** -->
+  <uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED" />
+
+  <!-- The application tag should already exist -->
+  <application android:icon="@drawable/icon" android:label="@string/app_name">
+    <activity android:name=".MainMenu" android:label="@string/app_name">
+      <intent-filter>
+        <action android:name="android.intent.action.MAIN" />
+          <category android:name="android.intent.category.LAUNCHER" />
+        </intent-filter>
+    </activity>
+
+    <!-- **** This is the metadata **** -->
+    <meta-data android:name="apploader.restartAfterReplace" android:value="true" />
+    <meta-data android:name="apploader.startOnBoot" android:value="true" />
+
+    <!-- **** This is the BroadcastReceiver **** -->
+    <receiver android:name="com.delphiworlds.kastri.DWMultiBroadcastReceiver">
+      <intent-filter>
+        <action android:name="android.intent.action.MY_PACKAGE_REPLACED"/>
+        <action android:name="android.intent.action.BOOT_COMPLETED"/>
+      </intent-filter>
+    </receiver>
+  </application>
+*/
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
