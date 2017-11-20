daedc1738ca705963c6c2aff64e31de817ca630b
DelphiWorlds <dave@davenottage.com>
2017-11-21 05:52:17 +1030

    Fixed compile path

--- Build/Tokyo/KastriFree.dpr
+++ Build/Tokyo/KastriFree.dpr
@@ -3,16 +3,20 @@ program KastriFree;
 uses
   System.StartUpCopy,
   FMX.Forms,
+  DW.DragDropLayout in '..\..\ComponentHelpers\DW.DragDropLayout.pas',
+  DW.DragListenerListView in '..\..\ComponentHelpers\DW.DragListenerListView.pas',
   DW.ElasticLayout in '..\..\ComponentHelpers\DW.ElasticLayout.pas',
-  DW.Canvas.GPU.Helpers in '..\..\Core\DW.Canvas.GPU.Helpers.pas',
+  DW.Base64.Helpers in '..\..\Core\DW.Base64.Helpers.pas',
+  DW.Forms.Helpers in '..\..\Core\DW.Forms.Helpers.pas',
   DW.Geodetic in '..\..\Core\DW.Geodetic.pas',
+  DW.Listeners in '..\..\Core\DW.Listeners.pas',
   DW.Messaging in '..\..\Core\DW.Messaging.pas',
-  DW.OSLog in '..\..\Core\DW.OSLog.pas',
   DW.Patch in '..\..\Core\DW.Patch.pas',
   DW.Precompile in '..\..\Core\DW.Precompile.pas',
   DW.REST.Json.Helpers in '..\..\Core\DW.REST.Json.Helpers.pas',
   DW.Services in '..\..\Core\DW.Services.pas',
-  DW.VirtualKeyboard.Helpers in '..\..\Core\DW.VirtualKeyboard.Helpers.pas';
+  DW.VirtualKeyboard.Helpers in '..\..\Core\DW.VirtualKeyboard.Helpers.pas',
+  DW.OSLog in '..\..\Core\DW.OSLog.pas';
 
 {$R *.res}
 
