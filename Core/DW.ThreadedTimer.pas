201d58234dfe44faa5d6fd2dc911b5866b797c0c
DelphiWorlds <dave@davenottage.com>
2017-12-18 20:30:43 +1030

    Created specifically to address https://quality.embarcadero.com/browse/RSP-19525, however could be used elsewhere

---
+++ Core/DW.ThreadedTimer.pas
@@ -0,0 +1,93 @@
+unit DW.ThreadedTimer;
+
+{*******************************************************}
+{                                                       }
+{                    Kastri Free                        }
+{                                                       }
+{          DelphiWorlds Cross-Platform Library          }
+{                                                       }
+{*******************************************************}
+
+{$I DW.GlobalDefines.inc}
+
+interface
+
+uses
+  // RTL
+  System.Classes,
+  // FMX
+  FMX.Types;
+
+type
+  TThreadedTimer = class(TTimer)
+  private
+    FThread: TThread;
+    procedure DoInterval;
+  protected
+    procedure CheckInterval;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+  end;
+
+  TTimerThread = class(TThread)
+  private
+    FTimer: TThreadedTimer;
+  protected
+    procedure Execute; override;
+  public
+    constructor Create(const ATimer: TThreadedTimer);
+  end;
+
+implementation
+
+{ TThreadedTimer }
+
+constructor TThreadedTimer.Create(AOwner: TComponent);
+begin
+  inherited;
+  Enabled := False;
+  FThread := TTimerThread.Create(Self);
+end;
+
+destructor TThreadedTimer.Destroy;
+begin
+  Enabled := False;
+  FThread.Free;
+  inherited;
+end;
+
+procedure TThreadedTimer.CheckInterval;
+begin
+  if Enabled then
+    DoInterval;
+end;
+
+procedure TThreadedTimer.DoInterval;
+begin
+  TThread.Queue(nil,
+    procedure
+    begin
+      DoOnTimer;
+    end
+  );
+end;
+
+{ TTimerThread }
+
+constructor TTimerThread.Create(const ATimer: TThreadedTimer);
+begin
+  inherited Create;
+  FTimer := ATimer;
+end;
+
+procedure TTimerThread.Execute;
+begin
+  while not Terminated do
+  begin
+    Sleep(FTimer.Interval);
+    FTimer.CheckInterval;
+  end;
+end;
+
+end.
