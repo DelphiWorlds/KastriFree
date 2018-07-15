94333cd544f73e6328ecf293dab4f4f54996eccf
DelphiWorlds <dave@davenottage.com>
2018-07-15 14:00:50 +0930

    Added Connectivity feature

---
+++ Features/Connectivity/DW.Connectivity.Android.pas
@@ -0,0 +1,121 @@
+unit DW.Connectivity.Android;
+
+{*******************************************************}
+{                                                       }
+{                    Kastri Free                        }
+{                                                       }
+{          DelphiWorlds Cross-Platform Library          }
+{                                                       }
+{*******************************************************}
+
+{$I DW.GlobalDefines.inc}
+
+interface
+
+uses
+  // Android
+  Androidapi.JNI.Net, Androidapi.JNI.GraphicsContentViewText,
+  // DW
+  DW.MultiReceiver.Android, DW.Connectivity;
+
+type
+  TPlatformConnectivity = class;
+
+  TConnectivityReceiver = class(TMultiReceiver)
+  private
+    FPlatformConnectivity: TPlatformConnectivity;
+  protected
+    procedure Receive(context: JContext; intent: JIntent); override;
+    procedure ConfigureActions; override;
+  public
+    constructor Create(const APlatformConnectivity: TPlatformConnectivity);
+  end;
+
+  TPlatformConnectivity = class(TObject)
+  private
+    FConnectivity: TConnectivity;
+    FReceiver: TConnectivityReceiver;
+  private
+    class function ConnectivityManager: JConnectivityManager; static;
+  protected
+    procedure ConnectivityChange;
+  public
+    class function IsConnectedToInternet: Boolean; static;
+    class function IsWifiInternetConnection: Boolean; static;
+  public
+    constructor Create(const AConnectivity: TConnectivity);
+    destructor Destroy; override;
+  end;
+
+implementation
+
+uses
+  // Android
+  Androidapi.JNI.JavaTypes, Androidapi.Helpers;
+
+type
+  TOpenConnectivity = class(TConnectivity);
+
+{ TConnectivityReceiver }
+
+constructor TConnectivityReceiver.Create(const APlatformConnectivity: TPlatformConnectivity);
+begin
+  inherited Create;
+  FPlatformConnectivity := APlatformConnectivity;
+end;
+
+procedure TConnectivityReceiver.ConfigureActions;
+begin
+  IntentFilter.addAction(TJConnectivityManager.JavaClass.CONNECTIVITY_ACTION);
+end;
+
+procedure TConnectivityReceiver.Receive(context: JContext; intent: JIntent);
+begin
+  FPlatformConnectivity.ConnectivityChange;
+end;
+
+{ TPlatformConnectivity }
+
+constructor TPlatformConnectivity.Create(const AConnectivity: TConnectivity);
+begin
+  inherited Create;
+  FConnectivity := AConnectivity;
+  FReceiver := TConnectivityReceiver.Create(Self);
+end;
+
+destructor TPlatformConnectivity.Destroy;
+begin
+  FReceiver.Free;
+  inherited;
+end;
+
+class function TPlatformConnectivity.ConnectivityManager: JConnectivityManager;
+var
+  LService: JObject;
+begin
+  LService := TAndroidHelper.Context.getSystemService(TJContext.JavaClass.CONNECTIVITY_SERVICE);
+  Result := TJConnectivityManager.Wrap(LService);
+end;
+
+class function TPlatformConnectivity.IsConnectedToInternet: Boolean;
+var
+  LInfo: JNetworkInfo;
+begin
+  LInfo := ConnectivityManager.getActiveNetworkInfo;
+  Result := (LInfo <> nil) and LInfo.isConnectedOrConnecting;
+end;
+
+class function TPlatformConnectivity.IsWifiInternetConnection: Boolean;
+var
+  LInfo: JNetworkInfo;
+begin
+  LInfo := ConnectivityManager.getActiveNetworkInfo;
+  Result := (LInfo <> nil) and (LInfo.getType = TJConnectivityManager.JavaClass.TYPE_WIFI) and LInfo.isConnectedOrConnecting;
+end;
+
+procedure TPlatformConnectivity.ConnectivityChange;
+begin
+  TOpenConnectivity(FConnectivity).DoConnectivityChange(IsConnectedToInternet);
+end;
+
+end.
---
+++ Features/Connectivity/DW.Connectivity.pas
@@ -0,0 +1,74 @@
+unit DW.Connectivity;
+
+{*******************************************************}
+{                                                       }
+{                    Kastri Free                        }
+{                                                       }
+{          DelphiWorlds Cross-Platform Library          }
+{                                                       }
+{*******************************************************}
+
+{$I DW.GlobalDefines.inc}
+
+interface
+
+type
+  TConnectivityChangeEvent = procedure(Sender: TObject; const IsConnected: Boolean) of object;
+
+  TConnectivity = class(TObject)
+  private
+    FPlatformConnectivity: TObject;
+    FOnConnectivityChange: TConnectivityChangeEvent;
+  protected
+    procedure DoConnectivityChange(const IsConnected: Boolean);
+  public
+    class function IsConnectedToInternet: Boolean;
+    class function IsWifiInternetConnection: Boolean;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    property OnConnectivityChange: TConnectivityChangeEvent read FOnConnectivityChange write FOnConnectivityChange;
+  end;
+
+implementation
+
+{$IF Defined(ANDROID)}
+uses
+  DW.Connectivity.Android;
+{$ENDIF}
+{$IF Defined(IOS)}
+uses
+  DW.Connectivity.iOS;
+{$ENDIF}
+
+{ TConnectivity }
+
+constructor TConnectivity.Create;
+begin
+  inherited;
+  FPlatformConnectivity := TPlatformConnectivity.Create(Self);
+end;
+
+destructor TConnectivity.Destroy;
+begin
+  FPlatformConnectivity.Free;
+  inherited;
+end;
+
+procedure TConnectivity.DoConnectivityChange(const IsConnected: Boolean);
+begin
+  if Assigned(FOnConnectivityChange) then
+    FOnConnectivityChange(Self, IsConnected);
+end;
+
+class function TConnectivity.IsConnectedToInternet: Boolean;
+begin
+  Result := TPlatformConnectivity.IsConnectedToInternet;
+end;
+
+class function TConnectivity.IsWifiInternetConnection: Boolean;
+begin
+  Result := TPlatformConnectivity.IsWifiInternetConnection;
+end;
+
+end.
