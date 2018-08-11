e6196eafb3e629711fdf0b22435131d35fbf5377
DelphiWorlds <dave@davenottage.com>
2018-08-11 15:00:52 +0930

    Uses clause cleanup

--- Demos/Notifications/Unit1.pas
+++ Demos/Notifications/Unit1.pas
@@ -4,8 +4,8 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Controls.Presentation, FMX.StdCtrls,
-  DW.Notifications, FMX.ScrollBox, FMX.Memo;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Controls.Presentation, FMX.StdCtrls, FMX.ScrollBox, FMX.Memo,
+  DW.Notifications;
 
 type
   TForm1 = class(TForm)
