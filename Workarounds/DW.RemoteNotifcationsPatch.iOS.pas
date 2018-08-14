457b0bc8efde2613f006de26e81aabfb0cadd285
DelphiWorlds <dave@davenottage.com>
2018-08-15 08:20:09 +0930

    Added patch for iOS for remote notifications

---
+++ Workarounds/DW.RemoteNotifcationsPatch.iOS.pas
@@ -0,0 +1,55 @@
+unit DW.RemoteNotifcationsPatch.iOS;
+
+interface
+
+implementation
+
+uses
+  System.SysUtils, iOSapi.Foundation, DW.iOSapi.UserNotifications;
+
+type
+  TRemoteNotificationsPatch = class(TObject)
+  private
+    class var FCurrent: TRemoteNotificationsPatch;
+    class destructor DestroyClass;
+  private
+    procedure RequestAuthorizationWithOptionsCompletionHandler(granted: Boolean; error: NSError);
+    procedure InternalRequestAuthorization;
+  protected
+    class procedure RequestAuthorization;
+  end;
+
+{ TRemoteNotificationsPatch }
+
+class destructor TRemoteNotificationsPatch.DestroyClass;
+begin
+  FCurrent.Free;
+end;
+
+procedure TRemoteNotificationsPatch.InternalRequestAuthorization;
+var
+  LOptions: UNAuthorizationOptions;
+begin
+  LOptions := UNAuthorizationOptionSound or UNAuthorizationOptionAlert or UNAuthorizationOptionBadge;
+  UserNotificationCenter.requestAuthorizationWithOptions(LOptions, RequestAuthorizationWithOptionsCompletionHandler);
+end;
+
+procedure TRemoteNotificationsPatch.RequestAuthorizationWithOptionsCompletionHandler(granted: Boolean; error: NSError);
+begin
+  //
+end;
+
+class procedure TRemoteNotificationsPatch.RequestAuthorization;
+begin
+  if TOSVersion.Check(10) then
+  begin
+    if FCurrent = nil then
+      FCurrent := TRemoteNotificationsPatch.Create;
+    FCurrent.InternalRequestAuthorization;
+  end;
+end;
+
+initialization
+  TRemoteNotificationsPatch.RequestAuthorization;
+
+end.
