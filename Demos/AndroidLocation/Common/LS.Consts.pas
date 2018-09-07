6dfebe2d74e9dd7adc905ca330908378d0296f8d
DelphiWorlds <dave@davenottage.com>
2018-09-08 08:55:30 +0930

    Added constants related to permissions

--- Demos/AndroidLocation/Common/LS.Consts.pas
+++ Demos/AndroidLocation/Common/LS.Consts.pas
@@ -5,6 +5,9 @@ interface
 const
   cServiceMessageAction = 'com.delphiworlds.action.servicemessage';
   cServiceMessageParamMessage = 'message';
+  cPermissionAccessCoarseLocation = 'android.permission.ACCESS_COARSE_LOCATION';
+  cPermissionAccessFineLocation = 'android.permission.ACCESS_FINE_LOCATION';
+  cRequestCodeLocation = 1;
 
 implementation
 
