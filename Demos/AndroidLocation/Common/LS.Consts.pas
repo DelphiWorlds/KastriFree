8baee3a1d121ac6c959e9ff41f7492250c87891a
DelphiWorlds <dave@davenottage.com>
2018-09-13 07:23:27 +0930

    Added constants for commands sent to service, refactored existing references

--- Demos/AndroidLocation/Common/LS.Consts.pas
+++ Demos/AndroidLocation/Common/LS.Consts.pas
@@ -3,8 +3,14 @@ unit LS.Consts;
 interface
 
 const
-  cServiceMessageAction = 'com.delphiworlds.action.servicemessage';
-  cServiceMessageParamMessage = 'message';
+  cServiceCommandAction = 'com.delphiworlds.action.SERVICE_COMMAND';
+  cServiceMessageAction = 'com.delphiworlds.action.SERVICE_MESSAGE';
+  cServiceStatusAction = 'com.delphiworlds.action.SERVICE_STATUS';
+  cServiceBroadcastParamMessage = 'MESSAGE';
+  cServiceBroadcastParamCommand = 'COMMAND';
+  cServiceCommandPause = 1;
+  cServiceCommandResume = 2;
+
   cPermissionAccessCoarseLocation = 'android.permission.ACCESS_COARSE_LOCATION';
   cPermissionAccessFineLocation = 'android.permission.ACCESS_FINE_LOCATION';
   cRequestCodeLocation = 1;
