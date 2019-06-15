0f390fa984013e807a51507635d1c8fb0ece9a4e
DelphiWorlds <dave@davenottage.com>
2019-06-15 16:12:46 +0930

    Removed unnecessary permissions stuff

--- Demos/FCMRevisited/MainFrm.pas
+++ Demos/FCMRevisited/MainFrm.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.ScrollBox, FMX.Memo, FMX.Controls.Presentation,
   FMX.StdCtrls, FMX.Layouts, FMX.Objects,
-  DW.Firebase.Messaging, DW.PermissionsRequester, DW.PermissionsTypes;
+  DW.Firebase.Messaging;
 
 type
   TfrmMain = class(TForm)
@@ -21,11 +21,10 @@ type
     procedure ClearMessagesButtonClick(Sender: TObject);
   private
     FFCM: TFirebaseMessaging;
-    // FRequester: TPermissionsRequester;
+    procedure CreateFirebaseMessaging;
     procedure FCMAuthorizationResultHandler(Sender: TObject; const AGranted: Boolean);
     procedure FCMTokenReceivedHandler(Sender: TObject; const AToken: string);
     procedure FCMMessageReceivedHandler(Sender: TObject; const APayload: TStrings);
-    procedure RequesterPermissionsResultHandler(Sender: TObject; const ARequestCode: Integer; const AResults: TPermissionResults);
   protected
     procedure Resize; override;
   public
@@ -41,37 +40,27 @@ implementation
 {$R *.fmx}
 
 uses
-  DW.Classes.Helpers, DW.OSLog, DW.UIHelper;
-
-const
-  cDangerousPermissions: array[0..4] of string = (
-    'android.permission.ACCESS_COARSE_LOCATION',
-    'android.permission.ACCESS_FINE_LOCATION',
-    'android.permission.CAMERA',
-    'android.permission.READ_EXTERNAL_STORAGE',
-    'android.permission.WRITE_EXTERNAL_STORAGE'
-  );
+  DW.OSLog, DW.UIHelper;
 
 { TfrmMain }
 
 constructor TfrmMain.Create(AOwner: TComponent);
 begin
   inherited;
+  CreateFirebaseMessaging;
+end;
+
+procedure TfrmMain.CreateFirebaseMessaging;
+begin
   FFCM := TFirebaseMessaging.Create;
   FFCM.OnAuthorizationResult := FCMAuthorizationResultHandler;
   FFCM.OnTokenReceived := FCMTokenReceivedHandler;
   FFCM.OnMessageReceived := FCMMessageReceivedHandler;
-  // The TPermissionsRequester in this demo is actually *optional*, i.e. the permissions it asks for are not actually required for push notifications
-  // FRequester := TPermissionsRequester.Create;
-  // FRequester.OnPermissionsResult := RequesterPermissionsResultHandler;
-  // FRequester.RequestPermissions(cDangerousPermissions, 1);
-  // If the permissions are not required by your app, you could omit all references to FRequester, and just call the next line:
   FFCM.RequestAuthorization;
 end;
 
 destructor TfrmMain.Destroy;
 begin
-  // FRequester.Free;
   FFCM.Free;
   inherited;
 end;
@@ -79,12 +68,6 @@ end;
 procedure TfrmMain.ClearMessagesButtonClick(Sender: TObject);
 begin
   MessagesMemo.Lines.Clear;
-end;
-
-procedure TfrmMain.RequesterPermissionsResultHandler(Sender: TObject; const ARequestCode: Integer; const AResults: TPermissionResults);
-begin
-  if (ARequestCode = 1) and AResults.AreAllGranted then
-    FFCM.RequestAuthorization;
 end;
 
 procedure TfrmMain.Resize;
@@ -103,6 +86,7 @@ end;
 procedure TfrmMain.FCMTokenReceivedHandler(Sender: TObject; const AToken: string);
 begin
   TokenMemo.Lines.Text := AToken;
+  // TokenMemo.Lines.Text := FFCM.DeviceToken;
   TOSLog.d('Token in FCMTokenReceivedHandler: %s', [AToken]);
 end;
 
