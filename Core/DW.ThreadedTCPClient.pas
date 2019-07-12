974639e890f379b4dbe0e1dae87619d8cf9b8840
DelphiWorlds <dave@davenottage.com>
2019-07-12 12:24:02 +0930

    Refactored to resolve deadlock issues Modified ReadData method to detect when connection is closed

--- Core/DW.ThreadedTCPClient.pas
+++ Core/DW.ThreadedTCPClient.pas
@@ -49,6 +49,11 @@ type
     FOnResponse: TResponseEvent;
     procedure ConnectClient;
     procedure DisconnectClient;
+    procedure DoConnected;
+    procedure DoDisconnected;
+    procedure DoException(const AException: Exception);
+    procedure DoReceiveData;
+    procedure DoResponse(const ACode: Integer; const AResponse: string);
     function GetConnectTimeout: Integer;
     function GetHost: string;
     function GetIsConnected: Boolean;
@@ -57,12 +62,8 @@ type
     procedure HandleException(const AException: Exception);
     function InternalConnect: Boolean;
     procedure InternalDisconnect;
-    procedure InternalDoConnected;
-    procedure InternalDoDisconnected;
-    procedure InternalDoException(const AException: Exception);
-    procedure InternalDoReceiveData;
-    procedure InternalDoResponse(const ACode: Integer; const AResponse: string);
     procedure InternalSendCmd;
+    function IsMainThread: Boolean;
     procedure ReadData;
     procedure ReadDataFromBuffer;
     procedure SendCmdFromClient;
@@ -73,12 +74,12 @@ type
     procedure TCPClientConnectedHandler(Sender: TObject);
     procedure TCPClientDisconnectedHandler(Sender: TObject);
   protected
-    procedure DoConnected; virtual;
-    procedure DoDisconnected; virtual;
-    procedure DoException(const AException: Exception); virtual;
-    procedure DoReceiveData; virtual;
-    procedure DoResponse(const ACode: Integer; const AResponse: string); virtual;
     procedure Execute; override;
+    procedure InternalDoConnected; virtual;
+    procedure InternalDoDisconnected; virtual;
+    procedure InternalDoException(const AException: Exception); virtual;
+    procedure InternalDoReceiveData; virtual;
+    procedure InternalDoResponse(const ACode: Integer; const AResponse: string); virtual;
     property Data: TBytes read FData;
     property OnConnected: TNotifyEvent read FOnConnected write FOnConnected;
     property OnDisconnected: TNotifyEvent read FOnDisconnected write FOnDisconnected;
@@ -137,10 +138,10 @@ end;
 
 destructor TCustomThreadedTCPClient.Destroy;
 begin
-  FTCPClient.Free;
   FConnectEvent.Free;
   FDisconnectEvent.Free;
   FSendCmdEvent.Free;
+  FTCPClient.Free;
   inherited;
 end;
 
@@ -189,6 +190,11 @@ begin
   FTCPClient.ReadTimeout := Value;
 end;
 
+function TCustomThreadedTCPClient.IsMainThread: Boolean;
+begin
+  Result := TThread.CurrentThread.ThreadID = MainThreadID;
+end;
+
 procedure TCustomThreadedTCPClient.TCPClientConnectedHandler(Sender: TObject);
 begin
   FIsConnected := True;
@@ -198,7 +204,6 @@ end;
 procedure TCustomThreadedTCPClient.TCPClientDisconnectedHandler(Sender: TObject);
 begin
   TOSLog.d('TCustomThreadedTCPClient.TCPClientDisconnectedHandler');
-  FIsConnected := False;
   DoDisconnected;
 end;
 
@@ -220,18 +225,16 @@ end;
 
 procedure TCustomThreadedTCPClient.DoConnected;
 begin
-  if Assigned(FOnConnected) then
-  begin
-    if FIsSynchronized then
-      Synchronize(InternalDoConnected)
-    else
-      InternalDoConnected;
-  end;
+  if FIsSynchronized and not IsMainThread then
+    Synchronize(InternalDoConnected)
+  else
+    InternalDoConnected;
 end;
 
 procedure TCustomThreadedTCPClient.InternalDoConnected;
 begin
-  FOnConnected(Self);
+  if Assigned(FOnConnected) then
+    FOnConnected(Self);
 end;
 
 procedure TCustomThreadedTCPClient.ConnectClient;
@@ -267,18 +270,17 @@ end;
 
 procedure TCustomThreadedTCPClient.DoDisconnected;
 begin
-  if Assigned(FOnDisconnected) then
-  begin
-    if FIsSynchronized then
-      Synchronize(InternalDoDisconnected)
-    else
-      InternalDoDisconnected;
-  end;
+  FIsConnected := False;
+  if FIsSynchronized and not IsMainThread then
+    Synchronize(InternalDoDisconnected)
+  else
+    InternalDoDisconnected;
 end;
 
 procedure TCustomThreadedTCPClient.InternalDoDisconnected;
 begin
-  FOnDisconnected(Self);
+  if Assigned(FOnDisconnected) then
+    FOnDisconnected(Self);
 end;
 
 procedure TCustomThreadedTCPClient.InternalSendCmd;
@@ -310,11 +312,18 @@ end;
 procedure TCustomThreadedTCPClient.ReadData;
 begin
   if FTCPClient.IOHandler <> nil then
-  try
-    ReadDataFromBuffer;
-  except
-    on E: Exception do
-      HandleException(E);
+  begin
+    if not FTCPClient.IOHandler.ClosedGracefully then
+    begin
+      try
+        ReadDataFromBuffer;
+      except
+        on E: Exception do
+          HandleException(E);
+      end;
+    end
+    else
+      DoDisconnected;
   end;
 end;
 
@@ -347,65 +356,58 @@ end;
 
 procedure TCustomThreadedTCPClient.DoException(const AException: Exception);
 begin
-  if Assigned(FOnException) then
+  if FIsSynchronized and not IsMainThread then
   begin
-    if FIsSynchronized then
-    begin
-      Synchronize(
-        procedure
-        begin
-          InternalDoException(AException);
-        end
-      );
-    end
-    else
-      InternalDoException(AException);
-  end;
+    Synchronize(
+      procedure
+      begin
+        InternalDoException(AException);
+      end
+    );
+  end
+  else
+    InternalDoException(AException);
 end;
-
 
 procedure TCustomThreadedTCPClient.InternalDoException(const AException: Exception);
 begin
-  FOnException(Self, AException);
+  if Assigned(FOnException) then
+    FOnException(Self, AException);
 end;
 
 procedure TCustomThreadedTCPClient.DoResponse(const ACode: Integer; const AResponse: string);
 begin
-  if Assigned(FOnResponse) then
+  if FIsSynchronized and not IsMainThread then
   begin
-    if FIsSynchronized then
-    begin
-      Synchronize(
-        procedure
-        begin
-          InternalDoResponse(ACode, AResponse);
-        end
-      );
-    end
-    else
-      InternalDoResponse(ACode, AResponse);
-  end;
+    Synchronize(
+      procedure
+      begin
+        InternalDoResponse(ACode, AResponse);
+      end
+    );
+  end
+  else
+    InternalDoResponse(ACode, AResponse);
 end;
 
 procedure TCustomThreadedTCPClient.InternalDoResponse(const ACode: Integer; const AResponse: string);
 begin
-  FOnResponse(Self, ACode, AResponse);
+  if Assigned(FOnResponse) then
+    FOnResponse(Self, ACode, AResponse);
 end;
 
 procedure TCustomThreadedTCPClient.DoReceiveData;
 begin
-  if Assigned(FOnReceiveData) then
-  begin
-    if FIsSynchronized then
-      Synchronize(InternalDoReceiveData)
-    else
-      InternalDoReceiveData;
-  end;
+  if FIsSynchronized and not IsMainThread then
+    Synchronize(InternalDoReceiveData)
+  else
+    InternalDoReceiveData;
 end;
 
 procedure TCustomThreadedTCPClient.InternalDoReceiveData;
 begin
-  FOnReceiveData(Self, FData);
+  if Assigned(FOnReceiveData) then
+    FOnReceiveData(Self, FData);
 end;
 
 procedure TCustomThreadedTCPClient.Execute;
