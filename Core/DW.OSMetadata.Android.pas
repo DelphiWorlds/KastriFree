d0cf7c6ee8821eef8a9f25001b24da562bd83e0a
DelphiWorlds <dave@davenottage.com>
2019-07-15 07:29:40 +0930

    Added support for retrieving basic "metadata"

---
+++ Core/DW.OSMetadata.Android.pas
@@ -0,0 +1,59 @@
+unit DW.OSMetadata.Android;
+
+{*******************************************************}
+{                                                       }
+{                    Kastri Free                        }
+{                                                       }
+{          DelphiWorlds Cross-Platform Library          }
+{                                                       }
+{*******************************************************}
+
+{$I DW.GlobalDefines.inc}
+
+interface
+
+uses
+  Androidapi.JNI.Os;
+
+type
+  TPlatformOSMetadata = record
+  private
+    class var FMetadata: JBundle;
+    class function GetMetadata: JBundle; static;
+  public
+    class function ContainsKey(const AKey: string): Boolean; static;
+    class function GetValue(const AKey: string; var AValue: string): Boolean; { overload; } static;
+  end;
+
+implementation
+
+uses
+  Androidapi.JNI.GraphicsContentViewText, Androidapi.Helpers;
+
+{ TPlatformOSMetadata }
+
+class function TPlatformOSMetadata.GetMetadata: JBundle;
+var
+  LFlags: Integer;
+begin
+  if FMetadata = nil then
+  begin
+    LFlags := TJPackageManager.JavaClass.GET_META_DATA;
+    FMetadata := TAndroidHelper.Context.getPackageManager.getApplicationInfo(TAndroidHelper.Context.getPackageName, LFlags).metaData;
+  end;
+  Result := FMetadata;
+end;
+
+class function TPlatformOSMetadata.ContainsKey(const AKey: string): Boolean;
+begin
+  Result := GetMetadata.containsKey(StringToJString(AKey));
+end;
+
+class function TPlatformOSMetadata.GetValue(const AKey: string; var AValue: string): Boolean;
+begin
+  Result := ContainsKey(AKey);
+  if Result then
+    AValue := JStringToString(GetMetadata.getString(StringToJString(AValue)));
+end;
+
+end.
---
+++ Core/DW.OSMetadata.pas
@@ -0,0 +1,48 @@
+unit DW.OSMetadata;
+
+{*******************************************************}
+{                                                       }
+{                    Kastri Free                        }
+{                                                       }
+{          DelphiWorlds Cross-Platform Library          }
+{                                                       }
+{*******************************************************}
+
+{$I DW.GlobalDefines.inc}
+
+interface
+
+type
+  TOSMetadata = record
+  public
+    class function ContainsKey(const AKey: string): Boolean; static;
+    class function GetValue(const AKey: string; var AValue: string): Boolean; { overload; } static;
+  end;
+
+implementation
+
+uses
+  // RTL
+  {$IF Defined(ANDROID)}
+  DW.OSMetadata.Android;
+  {$ELSEIF Defined(MACOS)}
+  DW.OSMetadata.Mac;
+  {$ELSEIF Defined(MSWINDOWS)}
+  DW.OSMetadata.Win;
+  {$ELSEIF Defined(LINUX)}
+  DW.OSMetadata.Linux;
+  {$ENDIF}
+
+{ TOSMetadata }
+
+class function TOSMetadata.ContainsKey(const AKey: string): Boolean;
+begin
+  Result := TPlatformOSMetadata.ContainsKey(AKey);
+end;
+
+class function TOSMetadata.GetValue(const AKey: string; var AValue: string): Boolean;
+begin
+  Result := TPlatformOSMetadata.GetValue(AKey, AValue);
+end;
+
+end.
