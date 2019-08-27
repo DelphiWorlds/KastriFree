722c3c4f1e1eb4496f5273da4cdad49f8e48c1b5
Dave Nottage <dave@davenottage.com>
2019-08-27 20:27:47 +0930

    Added consts file for Android

---
+++ Core/DW.Consts.Android.pas
@@ -0,0 +1,23 @@
+unit DW.Consts.Android;
+
+interface
+
+const
+  cPermissionAccessCoarseLocation = 'android.permission.ACCESS_COARSE_LOCATION';
+  cPermissionAccessFineLocation = 'android.permission.ACCESS_FINE_LOCATION';
+  cPermissionCamera = 'android.permission.CAMERA';
+  cPermissionReadContacts = 'android.permission.READ_CONTACTS';
+  cPermissionReadExternalStorage = 'android.permission.READ_EXTERNAL_STORAGE';
+  cPermissionReadPhoneState = 'android.permission.READ_PHONE_STATE';
+  cPermissionReadSMS = 'android.permission.READ_SMS';
+  cPermissionReceiveMMS = 'android.permission.RECEIVE_MMS';
+  cPermissionReceiveSMS = 'android.permission.RECEIVE_SMS';
+  cPermissionReceiveWAPPush = 'android.permission.RECEIVE_WAP_PUSH';
+  cPermissionRecordAudio = 'android.permission.RECORD_AUDIO';
+  cPermissionSendSMS = 'android.permission.SEND_SMS';
+  cPermissionUseFingerprint = 'android.permission.USE_FINGERPRINT';
+  cPermissionWriteExternalStorage = 'android.permission.WRITE_EXTERNAL_STORAGE';
+
+implementation
+
+end.
