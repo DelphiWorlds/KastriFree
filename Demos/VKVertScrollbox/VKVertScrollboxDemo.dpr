aa3ed63f7591a276477096bf133815dd0e0f40ea
Dave Nottage <dave@davenottage.com>
2019-11-05 08:41:25 +1030

    Fix paths, update for 10.3.2

--- Demos/VKVertScrollbox/VKVertScrollboxDemo.dpr
+++ Demos/VKVertScrollbox/VKVertScrollboxDemo.dpr
@@ -3,9 +3,7 @@ program VKVertScrollboxDemo;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  Unit1 in 'Unit1.pas' {frmMain},
-  DW.VKVertScrollbox in '..\..\ComponentHelpers\DW.VKVertScrollbox.pas',
-  OldDemoFrm in 'OldDemoFrm.pas' {frmOldDemo};
+  Unit1 in 'Unit1.pas' {frmMain};
 
 {$R *.res}
 
