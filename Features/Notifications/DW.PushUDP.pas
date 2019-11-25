772d44f946ad31ad64d307fba4691be6b0f2ba55
Dave Nottage <dave@davenottage.com>
2019-11-25 12:46:44 +1030

    Added PushUDP module
    Used for sending device/token info to anything listening on the same UDP port, such as the PushIt tool

---
+++ Features/Notifications/DW.PushUDP.pas
@@ -0,0 +1,83 @@
+unit DW.PushUDP;
+
+{*******************************************************}
+{                                                       }
+{                    Kastri Free                        }
+{                                                       }
+{          DelphiWorlds Cross-Platform Library          }
+{                                                       }
+{*******************************************************}
+
+{$I DW.GlobalDefines.inc}
+
+interface
+
+uses
+  // RTL
+  System.SysUtils, System.Classes,
+  // Indy
+  IdBaseComponent, IdComponent, IdUDPBase, IdUDPClient,
+  // FMX
+  FMX.Types;
+
+type
+  TPushUDP = class(TDataModule)
+    UDPClient: TIdUDPClient;
+    UDPTimer: TTimer;
+    procedure UDPTimerTimer(Sender: TObject);
+  private
+    FDeviceID: string;
+    FToken: string;
+    procedure SendDeviceInfo;
+  public
+    procedure UpdateDeviceInfo(const ADeviceID, AToken: string);
+  end;
+
+var
+  PushUDP: TPushUDP;
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+
+{$R *.dfm}
+
+uses
+  // RTL
+  System.JSON;
+
+procedure TPushUDP.UDPTimerTimer(Sender: TObject);
+begin
+  if not FToken.IsEmpty and not FDeviceID.IsEmpty then
+    SendDeviceInfo;
+end;
+
+procedure TPushUDP.SendDeviceInfo;
+var
+  LJSON: TJSONObject;
+  LOS: string;
+begin
+  if TOSVersion.Platform = TOSVersion.TPlatform.pfiOS then
+    LOS := 'IOS'
+  else if TOSVersion.Platform = TOSVersion.TPlatform.pfAndroid then
+    LOS := 'Android'
+  else
+    LOS := 'Unknown';
+  LJSON := TJSONObject.Create;
+  try
+    LJSON.AddPair('deviceid', FDeviceID);
+    LJSON.AddPair('token', FToken);
+    LJSON.AddPair('os', LOS);
+    UDPClient.Broadcast(LJSON.ToJSON, UDPClient.Port);
+  finally
+    LJSON.Free;
+  end;
+end;
+
+procedure TPushUDP.UpdateDeviceInfo(const ADeviceID, AToken: string);
+begin
+  FDeviceID := ADeviceID;
+  FToken := AToken;
+end;
+
+end.
