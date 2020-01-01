e5f207a6828b6557192472ded028432bdf4bd1e1
Dave Nottage <dave@davenottage.com>
2020-01-02 08:30:47 +1030

    Cross-platform location support

---
+++ Core/DW.Sensors.pas
@@ -0,0 +1,14 @@
+unit DW.Sensors;
+
+interface
+
+uses
+  System.Sensors;
+
+type
+  TLocationUsage = (Unauthorized, WhenInUse, Always);
+  TLocationActivity = (Other, Automotive, Fitness, Navigation);
+
+implementation
+
+end.
