b5ea63032b525a7adc717e52106a8f0de9570bf4
Dave Nottage <dave@davenottage.com>
2020-01-01 21:13:31 +1030

    Refactored GetTimeFromNowInMillis

--- Features/Notifications/DW.Notifications.Android.pas
+++ Features/Notifications/DW.Notifications.Android.pas
@@ -25,51 +25,6 @@ uses
   DW.Notifications, DW.MultiReceiver.Android, DW.Androidapi.JNI.App, DW.Androidapi.JNI.Support;
 
 type
-  JSystem = interface;
-
-  JSystemClass = interface(JObjectClass)
-    ['{0CDDA5AF-A679-4D83-A1DF-1B7C9F355E7B}']
-    {class} function _Geterr: JPrintStream; cdecl;
-    {class} function _Getin: JInputStream; cdecl;
-    {class} function _Getout: JPrintStream; cdecl;
-    {class} procedure arraycopy(src: JObject; srcPos: Integer; dst: JObject; dstPos: Integer; length: Integer); cdecl;
-    {class} function clearProperty(name: JString): JString; cdecl;
-    // {class} function console: JConsole; cdecl;
-    {class} function currentTimeMillis: Int64; cdecl;
-    {class} procedure exit(code: Integer); cdecl;
-    {class} procedure gc; cdecl;
-    {class} function getProperties: JProperties; cdecl;
-    {class} function getProperty(propertyName: JString): JString; cdecl; overload;
-    {class} function getProperty(name: JString; defaultValue: JString): JString; cdecl; overload;
-    // {class} function getSecurityManager: JSecurityManager; cdecl;
-    {class} function getenv(name: JString): JString; cdecl; overload;
-    {class} function getenv: JMap; cdecl; overload;
-    {class} function identityHashCode(anObject: JObject): Integer; cdecl;
-    {class} function inheritedChannel: JChannel; cdecl;
-    {class} function lineSeparator: JString; cdecl;
-    {class} procedure load(pathName: JString); cdecl;
-    {class} procedure loadLibrary(libName: JString); cdecl;
-    {class} function mapLibraryName(nickname: JString): JString; cdecl;
-    {class} function nanoTime: Int64; cdecl;
-    {class} procedure runFinalization; cdecl;
-    {class} procedure runFinalizersOnExit(flag: Boolean); cdecl;
-    {class} procedure setErr(newErr: JPrintStream); cdecl;
-    {class} procedure setIn(newIn: JInputStream); cdecl;
-    {class} procedure setOut(newOut: JPrintStream); cdecl;
-    {class} procedure setProperties(p: JProperties); cdecl;
-    {class} function setProperty(name: JString; value: JString): JString; cdecl;
-    // {class} procedure setSecurityManager(sm: JSecurityManager); cdecl;
-    {class} property err: JPrintStream read _Geterr;
-    {class} property &in: JInputStream read _Getin;
-    {class} property &out: JPrintStream read _Getout;
-  end;
-
-  [JavaSignature('java/lang/System')]
-  JSystem = interface(JObject)
-    ['{93E6C8D4-0481-439B-A258-870D01C85DF4}']
-  end;
-  TJSystem = class(TJavaGenericImport<JSystemClass, JSystem>) end;
-
   TNotificationReceiver = class(TMultiReceiver)
   private
     FNotifications: TNotifications;
@@ -145,12 +100,6 @@ uses
 
 type
   TOpenNotifications = class(TNotifications);
-
-// TODO: Move this to DW.Android.Helpers
-function GetTimeFromNowInMillis(const ASecondsFromNow: Int64): Int64;
-begin
-  Result := TJSystem.JavaClass.currentTimeMillis + (ASecondsFromNow * 1000);
-end;
 
 { TNotificationReceiver }
 
@@ -430,7 +379,7 @@ begin
   LID := GetUniqueID;
   StoreNotification(ANotification, LID);
   LPendingIntent := GetNotificationPendingIntent(ANotification, LID);
-  LTime := GetTimeFromNowInMillis(SecondsBetween(Now, ANotification.FireDate));
+  LTime := TAndroidHelperEx.GetTimeFromNowInMillis(SecondsBetween(Now, ANotification.FireDate));
   if TOSVersion.Check(6) then
     TAndroidHelper.AlarmManager.setExactAndAllowWhileIdle(TJAlarmManager.JavaClass.RTC_WAKEUP, LTime, LPendingIntent)
   else
