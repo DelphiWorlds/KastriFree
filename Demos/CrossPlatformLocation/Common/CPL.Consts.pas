0e639062d199e8769810548bd81a9def8a7f362f
Dave Nottage <dave@davenottage.com>
2020-01-02 23:55:01 +1030

    Added Cross-platform Location demo

---
+++ Demos/CrossPlatformLocation/Common/CPL.Consts.pas
@@ -0,0 +1,16 @@
+unit CPL.Consts;
+
+interface
+
+const
+  cServiceName = 'CrossPlatformLocationService';
+
+  cServiceCommandStartLocationUpdates = 100;
+  cServiceCommandStopLocationUpdates = 101;
+
+  cCloudLoggingHost = 'tcp://delphiworlds.com:7337';
+  cCloudLoggingName = 'CrossPlatformLocation';
+
+implementation
+
+end.
